1a926f20faa5f8c3f259989899b477f54d5fea4a
Çağrı (チャール) <116810433+Kharpoose@users.noreply.github.com>
2025-06-24 10:37:47 +0900

    test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
-export default function Home() {
+export default function ContactPage() {
   const router = useRouter();
 
   const buttonStyle = {
@@ -9,20 +9,16 @@ export default function Home() {
     color: 'black',
     border: '1px solid black',
     padding: '0.5rem 1rem',
-    marginRight: '1rem',
     cursor: 'pointer',
   };
 
   return (
     <main style={{ padding: '2rem' }}>
-      <h1>Main Page!</h1>
-      <p>You can switch pages with using buttons.</p>
+      <h1>Contact</h1>
+      <p>Contact us lilbro.</p>
 
-      <button onClick={() => router.push('/about')} style={buttonStyle}>
-        About
-      </button>
-      <button onClick={() => router.push('/contact')} style={buttonStyle}>
-        Contact
+      <button onClick={() => router.push('/')} style={buttonStyle}>
+        Back to Home
       </button>
     </main>
   );
